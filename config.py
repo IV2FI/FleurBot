8778abcd3bf98d5ec358e1194cf696abce609a93
IV2FI <iv2fi.pro@gmail.com>
2020-07-29 21:09:39 +0200

    debug

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import twitter
 
 def getApi(consumer_key, consumer_secret, access_token_key, access_token_secret):
-    return twitter.Api(consumer_key='XXX',
-                    consumer_secret='XXX',
-                    access_token_key='XXX',
-                    access_token_secret='XXX')
+    return twitter.Api(consumer_key=consumer_key,
+                    consumer_secret=consumer_secret,
+                    access_token_key=access_token_key,
+                    access_token_secret=access_token_secret)
